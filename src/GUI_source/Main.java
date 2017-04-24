4e32afdf1f0ecf36b310d9e10f7b0cc1201d74ad
testuser <s-kempinger@gmx.at>
2017-04-24 21:50:03 +0200

    test with transparent img

--- src/GUI_source/Main.java
+++ src/GUI_source/Main.java
@@ -2,8 +2,12 @@ package GUI_source;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -11,10 +15,18 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello Tester");
-        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.initStyle(StageStyle.TRANSPARENT);
+        Group root=new Group();
+
+        ImageView img=new ImageView();
+        img.setImage(new Image("file:test_java.png"));
+        HBox box =new HBox();
+        box.getChildren().add(img);
+        root.getChildren().add(box);
+        primaryStage.setTitle("Hello Tester");
+        primaryStage.setScene(new Scene(root));
+        Scene scene=
+    primaryStage.setF
         primaryStage.show();
     }
 
