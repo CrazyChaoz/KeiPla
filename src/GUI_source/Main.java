5aa2c41b21cdc05558e546568608cfa2378b25fe
testuser <s-kempinger@gmx.at>
2017-04-25 14:23:27 +0200

    small change

--- src/GUI_source/Main.java
+++ src/GUI_source/Main.java
@@ -36,7 +36,9 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-
+/*
+Some stuff PENIS
+ */
 
     public static void main(String[] args) {
         //comment
