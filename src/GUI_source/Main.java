785c4d26d50778b69ff002a15375e447734c0b84
testuser <s-kempinger@gmx.at>
2017-04-25 14:46:06 +0200

    Login_GUI made

--- src/GUI_source/Main.java
+++ src/GUI_source/Main.java
@@ -36,9 +36,6 @@ public class Main extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
     }
-/*
-Some stuff PENIS
- */
 
     public static void main(String[] args) {
         //comment
--- src/GUI_source/klasse.java
+++
@@ -1,7 +0,0 @@
-package GUI_source;
-
-/**
- * Created by Peter on 25.04.2017.
- */
-public class klasse {
-}
