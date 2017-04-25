6813325bb714e5b95357d1c1f934da63273da4bf
justCedric <akkro.noob@gmail.com>
2017-04-25 11:47:50 +0200

    test from cedLAPTOP

--- src/GUI_source/Main.java
+++ src/GUI_source/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello Tester");
+        primaryStage.setTitle("Hello Testcommit(ced)");
         primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
