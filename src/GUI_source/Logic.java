cc4b41b22989a98df60f88c777e25cc2e345fc69
testuser <s-kempinger@gmx.at>
2017-04-26 17:23:42 +0200

    Logic started

--- src/GUI_source/Logic.java
+++ src/GUI_source/Logic.java
@@ -7,6 +7,11 @@ public class Logic {
     public static void getQuestion(int hardness){
         switch(hardness){
             case 1:
+
+                break;
+            case 2:
+                break;
+            case 3:
                 break;
         }
     }
