47892c2f461e727ed7a263bb29bcaa7e6235bf36
testuser <s-kempinger@gmx.at>
2017-04-26 17:31:53 +0200

    Logic started

--- src/GUI_source/Logic.java
+++ src/GUI_source/Logic.java
@@ -1,5 +1,11 @@
 package GUI_source;
 
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.File;
+import java.util.Random;
+
 /**
  * Created by testuser on 26.04.2017.
  */
@@ -7,7 +13,18 @@ public class Logic {
     public static void getQuestion(int hardness){
         switch(hardness){
             case 1:
+                Random rndm=new Random();
+                File file = new File("ez\\"+rndm.nextInt(6) + 1+".xml");
+                DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+                org.w3c.dom.Document document=null;
+                try {
+                    DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+                    document = documentBuilder.parse(file);
+                }catch (Exception e){}
 
+
+                String usr = document.getElementsByTagName("user").item(0).getTextContent();
+                String pwd = document.getElementsByTagName("password").item(0).getTextContent();
                 break;
             case 2:
                 break;
