59905f6c0e173b7c60a11de83b11967bdfdbe88f
testuser <s-kempinger@gmx.at>
2017-04-26 19:46:57 +0200

    Pathprobleme, randomFilePicker impl

--- src/GUI_source/Logic.java
+++ src/GUI_source/Logic.java
@@ -13,39 +13,45 @@ import java.util.Scanner;
 public class Logic {
     public static String[] getQuestion(int hardness){
 
-        String[]retournage=new String[5];
-        int wh=1;
-        while(wh==1){
-            switch(hardness){
-                case 1:
-                    wh=0;
-                    Random rndm=new Random();
-                    File file = new File(   "res\\questions\\ez\\"+
-                                            rndm.nextInt(getFileNumber(new File("res\\questions\\ez\\")))+1+
-                                            ".xml");
-                    DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-                    org.w3c.dom.Document document=null;
-                    try {
-                        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-                        document = documentBuilder.parse(file);
-                        retournage[0] = document.getElementsByTagName("question").item(0).getTextContent();
-                        retournage[1] = document.getElementsByTagName("answer").item(0).getTextContent();
-                        retournage[2] = document.getElementsByTagName("answer").item(1).getTextContent();
-                        retournage[3] = document.getElementsByTagName("answer").item(2).getTextContent();
-                        retournage[4] = document.getElementsByTagName("answer").item(3).getTextContent();
-                    }catch (Exception e){
-                        e.printStackTrace();
-                        wh=1;
-                    }
+        String[] retournage=null;
+        Random rndm=new Random();
 
-                    break;
-                case 2:
-                    break;
-                case 3:
-                    break;
-            }
+        switch(hardness){
+            case 1:
+                getXMLInhalt(new File("res\\questions\\ez\\"+rndm.nextInt(getFileNumber(new File("res\\questions\\ez\\")))+1+".xml"));
+                break;
+            case 2:
+                getXMLInhalt(new File("res\\questions\\mid\\"+rndm.nextInt(getFileNumber(new File("res\\questions\\mid\\")))+1+".xml"));
+                break;
+            case 3:
+                getXMLInhalt(new File("res\\questions\\hard\\"+rndm.nextInt(getFileNumber(new File("res\\questions\\hard\\")))+1+".xml"));
+                break;
         }
 
+
+        return retournage;
+    }
+
+    public static String[] getXMLInhalt(File file){
+        int wh=1;
+        String[]retournage=new String[5];
+
+        while(wh==1){
+            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+            org.w3c.dom.Document document;
+            try {
+                DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+                document = documentBuilder.parse(file);
+                retournage[0] = document.getElementsByTagName("question").item(0).getTextContent();
+                retournage[1] = document.getElementsByTagName("answer").item(0).getTextContent();
+                retournage[2] = document.getElementsByTagName("answer").item(1).getTextContent();
+                retournage[3] = document.getElementsByTagName("answer").item(2).getTextContent();
+                retournage[4] = document.getElementsByTagName("answer").item(3).getTextContent();
+            }catch (Exception e){
+                e.printStackTrace();
+                wh=1;
+            }
+        }
         return retournage;
     }
 
