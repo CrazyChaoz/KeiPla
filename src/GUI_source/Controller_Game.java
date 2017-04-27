33fb5188867bf4ce0fbfd038039a9c09b91fc40b
testuser <s-kempinger@gmx.at>
2017-04-27 16:35:19 +0200

    Controler configured

--- src/GUI_source/Controller_Game.java
+++ src/GUI_source/Controller_Game.java
@@ -31,28 +31,35 @@ public class Controller_Game implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        setText();
 
         this.answer1.setOnAction(event -> {
             selected=UI_FXML.currQuestion[1];
-
+            System.out.println(selected);
         });
 
         this.answer2.setOnAction(event -> {
             selected=UI_FXML.currQuestion[2];
+            System.out.println(selected);
         });
 
         this.answer3.setOnAction(event -> {
             selected=UI_FXML.currQuestion[3];
+            System.out.println(selected);
         });
 
         this.answer4.setOnAction(event -> {
             selected=UI_FXML.currQuestion[4];
+            System.out.println(selected);
         });
 
         this.submit.setOnAction(event -> {
             if(UI_FXML.currQuestion[Integer.parseInt(UI_FXML.currQuestion[5])].equals(selected)){
                 UI_FXML.score++;
-                newQuestion();
+                UI_FXML.currQuestion=Logic.randomFilePicker(1);
+                try {
+                    UI_FXML.currStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("Ingame.fxml"))));
+                } catch(IOException e){}
             }else{
                 try {
                     UI_FXML.currStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("MainMenu.fxml"))));
@@ -65,17 +72,14 @@ public class Controller_Game implements Initializable {
     }
 
 
-    public void newQuestion(){
-        try {
-            UI_FXML.currQuestion=Logic.randomFilePicker(hardness);
-        }catch (Exception e){
+    public void setText(){
 
-            this.question.setText(UI_FXML.currQuestion[0]);
-            this.answer1.setText(UI_FXML.currQuestion[1]);
-            this.answer2.setText(UI_FXML.currQuestion[2]);
-            this.answer3.setText(UI_FXML.currQuestion[3]);
-            this.answer4.setText(UI_FXML.currQuestion[4]);
-        }
+        this.question.setText(UI_FXML.currQuestion[0]);
+        this.answer1.setText(UI_FXML.currQuestion[1]);
+        this.answer2.setText(UI_FXML.currQuestion[2]);
+        this.answer3.setText(UI_FXML.currQuestion[3]);
+        this.answer4.setText(UI_FXML.currQuestion[4]);
+
 
     }
 }
--- src/GUI_source/Logic.java
+++ src/GUI_source/Logic.java
@@ -20,47 +20,47 @@ import java.util.Random;
 import org.w3c.dom.Document;
 
 public class Logic {
+    private static int wh=1;
+
     public static String[] randomFilePicker(int hardness){
 
         String[] retournage=null;
         Random rndm=new Random();
 
-        switch(hardness){
-            case 1:
-                retournage=getXMLInhalt(new File("res"+ File.separator+"questions"+ File.separator+"ez"+File.separator+rndm.nextInt(getFileNumber(new File("res"+ File.separator+"questions"+ File.separator+"ez"+File.separator)))+1+".xml"));
-                break;
-            case 2:
-                retournage=getXMLInhalt(new File("res"+ File.separator+"questions"+ File.separator+"mid"+ File.separator+rndm.nextInt(getFileNumber(new File("res"+ File.separator+"questions"+ File.separator+"mid"+ File.separator)))+1+".xml"));
-                break;
-            case 3:
-                retournage=getXMLInhalt(new File("res"+ File.separator+"questions"+ File.separator+"hard"+ File.separator+rndm.nextInt(getFileNumber(new File("res"+ File.separator+"questions"+ File.separator+"hard"+ File.separator)))+1+".xml"));
-                break;
+        while(wh==1){
+            wh=0;
+            switch(hardness){
+                case 1:
+                    retournage=getXMLInhalt(new File("res"+ File.separator+"questions"+ File.separator+"ez"+File.separator+rndm.nextInt(getFileNumber(new File("res"+ File.separator+"questions"+ File.separator+"ez"+File.separator)))+1+".xml"));
+                    break;
+                case 2:
+                    retournage=getXMLInhalt(new File("res"+ File.separator+"questions"+ File.separator+"mid"+ File.separator+rndm.nextInt(getFileNumber(new File("res"+ File.separator+"questions"+ File.separator+"mid"+ File.separator)))+1+".xml"));
+                    break;
+                case 3:
+                    retournage=getXMLInhalt(new File("res"+ File.separator+"questions"+ File.separator+"hard"+ File.separator+rndm.nextInt(getFileNumber(new File("res"+ File.separator+"questions"+ File.separator+"hard"+ File.separator)))+1+".xml"));
+                    break;
+            }
         }
         return retournage;
     }
 
     public static String[] getXMLInhalt(File file){
-        int wh=1;
         String[]retournage=new String[6];
-
-        while(wh==1){
-            wh=0;
-            DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
-            Document document;
-            try {
-                DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
-                document = documentBuilder.parse(file);
-                retournage[0] = document.getElementsByTagName("question").item(0).getTextContent();
-                retournage[1] = document.getElementsByTagName("answer").item(0).getTextContent();
-                retournage[2] = document.getElementsByTagName("answer").item(1).getTextContent();
-                retournage[3] = document.getElementsByTagName("answer").item(2).getTextContent();
-                retournage[4] = document.getElementsByTagName("answer").item(3).getTextContent();
-                retournage[5] = document.getElementsByTagName("right").item(0).getTextContent();
-            }catch (Exception e){
-                e.printStackTrace();
-                wh=1;
-            }
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory.newInstance();
+        Document document;
+        try {
+            DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+            document = documentBuilder.parse(file);
+            retournage[0] = document.getElementsByTagName("question").item(0).getTextContent();
+            retournage[1] = document.getElementsByTagName("answer").item(0).getTextContent();
+            retournage[2] = document.getElementsByTagName("answer").item(1).getTextContent();
+            retournage[3] = document.getElementsByTagName("answer").item(2).getTextContent();
+            retournage[4] = document.getElementsByTagName("answer").item(3).getTextContent();
+            retournage[5] = document.getElementsByTagName("right").item(0).getTextContent();
+        }catch (Exception e){
+            wh=1;
         }
+
         return retournage;
     }
 
