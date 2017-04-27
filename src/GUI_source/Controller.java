3809f9f1879ad60f6f911e8c9b0094575529f995
testuser <s-kempinger@gmx.at>
2017-04-27 13:19:57 +0200

    fxml

--- src/GUI_source/Controller.java
+++ src/GUI_source/Controller.java
@@ -1,4 +1,23 @@
 package GUI_source;
 
-public class Controller {
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+
+    @FXML
+    private Button singleplayer;
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+        this.singleplayer.setOnAction(event -> {
+
+        });
+    }
 }
