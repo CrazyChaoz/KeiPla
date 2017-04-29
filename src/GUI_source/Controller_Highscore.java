a2b454ab8ec60b78d9e1bd6a38fb34115f8c8157
testuser <s-kempinger@gmx.at>
2017-04-29 15:28:25 +0200

    added highscore

--- src/GUI_source/Controller_Highscore.java
+++ src/GUI_source/Controller_Highscore.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
@@ -18,61 +19,70 @@ import java.util.ResourceBundle;
 
 
 public class Controller_Highscore implements Initializable {
-    private static String selected=null;
     private static double xOffset;
     private static double yOffset;
 
     @FXML
-    private Button score1;
+    private Label score1;
     @FXML
-    private Button score2;
+    private Label score2;
     @FXML
-    private Button score3;
+    private Label score3;
     @FXML
-    private Button score4;
+    private Label score4;
     @FXML
-    private Button score5;
+    private Label score5;
     @FXML
     private Button close;
     @FXML
     private Button minimize;
     @FXML
     private Button title;
+    @FXML
+    private Button back;
 
     @Override
     public void initialize(URL location, ResourceBundle resources){
         setText();
 
-        title.setOnMousePressed(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                xOffset = UI_FXML.currStage.getX() - event.getScreenX();
-                yOffset = UI_FXML.currStage.getY() - event.getScreenY();
-            }
+        title.setOnMousePressed(event -> {
+            xOffset = UI_FXML.currStage.getX() - event.getScreenX();
+            yOffset = UI_FXML.currStage.getY() - event.getScreenY();
         });
 
-        title.setOnMouseDragged(new EventHandler<MouseEvent>() {
-            @Override
-            public void handle(MouseEvent event) {
-                UI_FXML.currStage.setX(event.getScreenX() + xOffset);
-                UI_FXML.currStage.setY(event.getScreenY() + yOffset);
-            }
+        title.setOnMouseDragged(event -> {
+            UI_FXML.currStage.setX(event.getScreenX() + xOffset);
+            UI_FXML.currStage.setY(event.getScreenY() + yOffset);
         });
+
         this.minimize.setOnAction(event -> {
             UI_FXML.currStage.setIconified(true);
         });
+
         this.close.setOnAction(event -> {
             UI_FXML.currStage.close();
+        });
+
+        this.back.setOnAction(event -> {
+            try {
+                UI_FXML.currStage.close();
+                UI_FXML.currStage=new Stage(StageStyle.TRANSPARENT);
+                UI_FXML.currStage.setTitle("Hauptmenü");
+                UI_FXML.currStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("MainMenu.fxml"))));
+                UI_FXML.currStage.getIcons().add(new Image(this.getClass().getResourceAsStream("res"+ File.separator+"KeiPla-Icon-128.png")));
+                setText();
+                UI_FXML.currStage.show();
+            } catch(IOException e){}
         });
 
     }
 
 
     public void setText(){
-        this.score1.setText(UI_FXML.currQuestion[0]);
-        this.score2.setText(UI_FXML.currQuestion[1]);
-        this.score3.setText(UI_FXML.currQuestion[2]);
-        this.score4.setText(UI_FXML.currQuestion[3]);
-        this.score5.setText(UI_FXML.currQuestion[4]);
+        this.score1.setText("");
+        this.score2.setText("");
+        this.score3.setText("");
+        this.score4.setText("");
+        this.score5.setText("");
     }
 }
