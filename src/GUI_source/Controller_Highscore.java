4c8d68e6ec2fa3ab291441a31267b795c869c974
testuser <s-kempinger@gmx.at>
2017-04-29 15:29:54 +0200

    added highscore

--- src/GUI_source/Controller_Highscore.java
+++ src/GUI_source/Controller_Highscore.java
@@ -1,6 +1,5 @@
 package GUI_source;
 
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -8,7 +7,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
-import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
