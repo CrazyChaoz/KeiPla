c11fc7cc919c75e7c211d2f5733cae002a9184c7
kemp <s-kempinger@gmx.at>
2017-09-16 20:19:32 +0200

    Protocol w/ sent name, server is not a player anymore

--- src/GUI_source/SERVER_NO_GUI.java
+++ src/GUI_source/SERVER_NO_GUI.java
@@ -17,6 +17,8 @@ import java.util.Scanner;
 /**
  * Created by kemp on 16.09.17.
  * KEIPLA SERVER
+ * ENDLESS MODE ONLY
+ * Protocol updated
  */
 public class SERVER_NO_GUI{
     public static void main(String[] args) {
@@ -24,18 +26,15 @@ public class SERVER_NO_GUI{
     }
 
     private int wh=1;
-    private String multi_result=null;
-    private int score=0;
     private String[] currQuestion=new String[6];
 
     public SERVER_NO_GUI() {
         int port=63956;
-        List<BufferedReader> instreams=new ArrayList<>();
-        List<PrintWriter> outstreams=new ArrayList<>();
-        List<String> inputLines=new ArrayList<>();
+
+        List<Client> clients=new ArrayList<>();
 
 
-        String version="1.0";
+        String version="1.1";
 
 
         System.out.println("Welcome to KeiPla_Server v"+version);
@@ -52,17 +51,26 @@ public class SERVER_NO_GUI{
                 ServerSocket serverSocket = new ServerSocket(port);
 
         ){
-            int i=0;
-            while(i<numClients) {
+            while(clients.size()<numClients) {
                 Socket clientSocket = serverSocket.accept();
 
-                outstreams.add(new PrintWriter(clientSocket.getOutputStream(), true));
-                instreams.add(new BufferedReader(new InputStreamReader(clientSocket.getInputStream())));
-                i++;
+                clients.add(
+                    new Client(
+                        new PrintWriter(clientSocket.getOutputStream(), true),
+                        new BufferedReader(new InputStreamReader(clientSocket.getInputStream()))
+                    )
+                );
             }
 
-            String inputLine, outputLine=null;
+            String outputLine=null;
 
+            for(Client c:clients){
+                String inpLn=c.in.readLine();
+                if(inpLn==null)
+                    break;
+                else
+                    c.name=inpLn;
+            }
 
             System.out.println("Host connected");
             newQ(1);
@@ -73,70 +81,68 @@ public class SERVER_NO_GUI{
                             currQuestion[3]+";"+
                             currQuestion[4]+";"+
                             "1337"+"\n";
-            for (PrintWriter out:outstreams)
-                out.println(outputLine);
+            for (Client c:clients)
+                c.out.println(outputLine);
 
 
 
             System.out.println("waiting");
 
-            for (PrintWriter out:outstreams)
-                out.println("waiting");
+            for (Client c:clients)
+                c.out.println("waiting");
 
 
             while (true) {
-                for(BufferedReader in:instreams)
-                    if((inputLine = in.readLine())==null)
-                        break;
 
-                System.out.println("Client: "+inputLine);
-                System.out.println("Server: "+multi_result);
+                List<String> inpMsgs=new ArrayList<>();
 
-                Boolean otherSolution;
-                Boolean ownSolution;
+                for(Client c:clients){
+                    String inpLn=c.in.readLine();
+                    if(inpLn==null)
+                        break;
+                    else
+                        inpMsgs.add(inpLn);
+                }
 
-                if(!inputLine.equals("waiting")&&multi_result!=null){
-                    otherSolution = inputLine.equals(currQuestion[Integer.parseInt(currQuestion[5])]);
-                    ownSolution = multi_result.equals(currQuestion[Integer.parseInt(currQuestion[5])]);
-                    multi_result=null;
-                    if (otherSolution && ownSolution){
-                        System.out.println("Both Right");
-                        newQ(1);
-                        outputLine =
-                                currQuestion[0] + ";" +
-                                        currQuestion[1] + ";" +
-                                        currQuestion[2] + ";" +
-                                        currQuestion[3] + ";" +
-                                        currQuestion[4] + ";" +
-                                        "1337" + "\n";
-                        score++;
-                    }else if (!otherSolution && ownSolution){
-                        System.out.println("You Won");
-                        multi_result = "You Won";
-                        out.println("You Lost");
-                        Thread.sleep(1200);
-                        out.println("You Lost");
-                        Thread.sleep(1200);
-                        break;
-                    }else if (otherSolution && !ownSolution){
-                        System.out.println("You Lost");
-                        multi_result = "You Lost";
-                        out.println("You Won");
-                        Thread.sleep(1200);
-                        out.println("You Won");
-                        Thread.sleep(1200);
-                        break;
-                    }else if(!otherSolution&&!ownSolution){
-                        System.out.println("Both Wrong");
-                        multi_result = "Both Lost";
-                        out.println("Both Wrong");
-                        Thread.sleep(1200);
-                        out.println("Both Wrong");
-                        Thread.sleep(1200);
-                        break;
-                    }else if (inputLine.equals("stop_communication")) {
-                        break;
+                for(String lastMsg:inpMsgs)
+                    System.out.println("Client: "+lastMsg);
+
+                System.out.println("Server: "+currQuestion[Integer.parseInt(currQuestion[5])]);
+
+
+
+                //### stop communication###
+                //### send score to all ###
+                if(inpMsgs.contains("see_tat_shieeet")){
+                    for (Client c:clients) {
+                        c.out.println();
                     }
+                    Thread.sleep(1200);
+                    for (Client c:clients) {
+                        c.out.println();
+                    }
+                    Thread.sleep(1200);
+                    break;
+                }
+
+
+                if(!inpMsgs.contains("waiting")){
+
+                    for(int i=0;i<clients.size();i++){
+                        if(inpMsgs.get(i).equals(currQuestion[Integer.parseInt(currQuestion[5])]))
+                            clients.get(i).score++;
+                        System.out.println("Client: "+clients.get(i).name+"\nScore: "+clients.get(i).score);
+                    }
+
+                    newQ(1);
+                    outputLine =
+                            currQuestion[0] + ";" +
+                            currQuestion[1] + ";" +
+                            currQuestion[2] + ";" +
+                            currQuestion[3] + ";" +
+                            currQuestion[4] + ";" +
+                            "1337" + "\n";
+
                 }else{
                     System.out.println("waiting");
                     outputLine="waiting";
@@ -204,5 +210,20 @@ public class SERVER_NO_GUI{
 
     public int getFileNumber(File folder){
         return folder.list().length;
+    }
+}
+
+class Client{
+
+    public PrintWriter out;
+    public BufferedReader in;
+
+    public String name;
+    public int score;
+
+    public Client(PrintWriter out, BufferedReader in) {
+        this.out = out;
+        this.in = in;
+        score=0;
     }
 }
